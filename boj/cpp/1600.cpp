7697559debe3dddbca7c672f1a200195fe409c5c
JinwooJeon1024 <104386015+JinwooJeon1024@users.noreply.github.com>
2024-11-14 12:28:10 +0000

    solving 1600

--- boj/cpp/1600.cpp
+++ boj/cpp/1600.cpp
@@ -3,8 +3,6 @@ using namespace std;
 
 int dis[202][202];
 bool vis[202][202];
-int dis2[202][202];
-bool vis2[202][202];
 
 int w, h, k;
 int board[202][202];
@@ -16,79 +14,49 @@ int dx2[4] = {1, -1, 0, 0};
 int dy2[4] = {0, 0, 1, -1};
 
 void bfs(int r, int c) {
-  queue<pair<int, int>> q;
+  queue<tuple<int, int, int>> q;
 
-  q.push({r, c});
+  q.push(make_tuple(r, c, k));
   vis[r][c] = 1;
 
-  int horse = k;
-
-  while(!q.empty() && horse--) {
-    pair<int, int> cur = q.front();
+  while(!q.empty()) {
+    auto cur = q.front();
     q.pop();
 
-    for(int i = 0; i < 8; i++) {
-      int nx = cur.first + dx[i];
-      int ny = cur.second + dy[i];
+    for(int dir = 0; dir < 8; dir++) {
+      int nx = get<0>(cur) + dx[dir];
+      int ny = get<1>(cur) + dy[dir];
+      int nk = get<2>(cur) - 1;
 
       if(vis[nx][ny]) continue;
-      if(nx < 0 || nx >= w || ny < 0 || ny >= h) continue;
+      if(nx < 0 || nx >= w || ny < 0 || ny >= h || nk < 0) continue;
       if(board[nx][ny] == 1) continue;
 
-      dis[nx][ny] = dis[cur.first][cur.second] + 1;
+      dis[nx][ny] = dis[get<0>(cur)][get<1>(cur)] + 1;
       vis[nx][ny] = 1;
-      q.push({nx, ny});
+      q.push(make_tuple(nx, ny, nk));
     }
-  }
-}
+    
+    for(int dir = 0; dir < 4; dir++) {
+      int nx = get<0>(cur) + dx2[dir];
+      int ny = get<1>(cur) + dy2[dir];
+      int nk = get<2>(cur);
 
-void bfs2(int r, int c) {
-  queue<pair<int, int>> q;
-
-  q.push({r, c});
-  vis2[r][c] = 1;
-
-  while(!q.empty()) {
-    pair<int, int> cur = q.front();
-    q.pop();
-
-    for(int i = 0; i < 4; i++) {
-      int nx = cur.first + dx2[i];
-      int ny = cur.second + dy2[i];
-
-      if(vis2[nx][ny]) continue;
-      if(nx < 0 || nx >= w || ny < 0 || ny >= h) continue;
+      if(vis[nx][ny]) continue;
+      if(nx < 0 || nx >= w || ny < 0 || ny >= h || nk < 0) continue;
       if(board[nx][ny] == 1) continue;
 
-      dis2[nx][ny] = dis2[cur.first][cur.second] + 1;
-      vis2[nx][ny] = 1;
-      q.push({nx, ny});
+      dis[nx][ny] = dis[get<0>(cur)][get<1>(cur)] + 1;
+      vis[nx][ny] = 1;
+      q.push(make_tuple(nx, ny, nk));
     }
   }
-}
-
-int ans() {
-  int ret = INT_MAX;
-  bool find = 0;
-
-  for(int i = 0; i < h; i++) {
-    for(int j = 0; j < w; j++) {
-      if(dis[i][j] >= 0 && dis2[i][j] >= 0){
-        ret = min(ret, dis[i][j] + dis2[i][j]);
-        find = 1;
-      } 
-    }
-  }
-  
-  if(find) return ret;
-  else return -1;
 }
 
 void solve() {
   bfs(0, 0);
-  bfs2(h-1, w-1);
 
-  cout << ans();
+  cout << dis[h-1][w-1];
 }
 
 int main(void) {
@@ -99,30 +67,19 @@ int main(void) {
 
   memset(dis, -1, sizeof(dis));
   dis[0][0] = 0;
-  memset(dis2, -1, sizeof(dis2));
-  dis2[h-1][w-1] = 0;
 
   for(int i = 0; i < h; i++)
     for(int j = 0; j < w; j++) {
       cin >> board[i][j];
     }
-
   
   solve();
 
-  // cout << '\n';
-  // for(int i = 0; i < h; i++) {
-  //   for(int j = 0; j < w; j++) {
-  //     cout << dis[i][j] << ' ';
-  //   }
-  //   cout << '\n';
-  // }
-
-  // cout << '\n';
-  // for(int i = 0; i < h; i++) {
-  //   for(int j = 0; j < w; j++) {
-  //     cout << dis2[i][j] << ' ';
-  //   }
-  //   cout << '\n';
-  // }
+  cout << '\n';
+  for(int i = 0; i < h; i++) {
+    for(int j = 0; j < w; j++) {
+      cout << dis[i][j] << ' ';
+    }
+    cout << '\n';
+  }
 }
